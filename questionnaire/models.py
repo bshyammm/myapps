6d58fcfc55737d10b7ee62afa9a01e866e91d498
Shyam Banuprakash <bshyamm@icloud.com>
2018-06-09 19:55:14 -0700

    update models

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -2,14 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+class Questionnaire(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=2500)
+    active = models.NullBooleanField()
+
+    def __str__(self):
+        return self.name
+
 class Question(models.Model):
     choices = {
         ('Multiple Choice', 'Multiple Choice'),
         ('Freetext', 'Freetext'),
     }
+    questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
     question = models.CharField("Question Text", max_length=250)
     question_type = models.CharField("Question Type", max_length=250, choices = choices)
     position = models.IntegerField("Question Position", default=0)
+    active = models.NullBooleanField()
 
     def __str__(self):
         return self.question
@@ -17,6 +27,7 @@ class Question(models.Model):
 class Response(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     response = models.CharField("Response text", max_length=250)
+    active = models.NullBooleanField()
 
     def __str__(self):
         return self.response
--- questionnaire/views.py
+++ questionnaire/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse ("Questionnaire app")
 
-    
+def questionnaires(request):
+    return HttpResponse("view to display all questionnaires, and to allow to create a new one")
+
+def questions(request, questionnaire_id):
+    return HttpResponse("view to display questions in a questionnaire %s.", %questionnaire_id)
